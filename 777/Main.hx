783ae868bf46558e5a3b151f7018158c02935416
NevrUSB <CNQBAS@GMAIL.COM>
2017-03-30 17:47:52 +0700

    -allert
    // вычисляем высоту окна

--- 777/Main.hx
+++ 777/Main.hx
@@ -7,58 +7,64 @@ import openfl.Lib;
 import openfl.events.MouseEvent;
 import js.Lib;
 
-class Main extends Sprite 
+class Main extends Sprite
 {
-	public static function main() {
-		Browser.alert(Browser.window.innerWidth + 'x' + Browser.window.innerHeight);
-		Browser.alert(Math.round(Math.random() * 0xFFFFFF));
-		
-		var spriteWidth:Int = 50;
-		var spriteHeight:Int = 50;
-		var maxXPosition:Int = Math.round(Browser.window.innerWidth) - spriteWidth;
-		var maxYPosition:Int = Math.round(Browser.window.innerHeight) - spriteHeight;
-		
-		for (i in 0...100) {
-			var x1:Int = Std.random(maxXPosition);
-			var x2:Int = x1 + 50;
-			var y1:Int = Std.random(maxYPosition);
-			var y2:Int = y1 + 50;
-			openfl.Lib.current.addChild(new Kvadrat(x1, y1, x2, y2));
+public static function main() {
+var count:Int = 5;
+
+	
+	var maxXPosition:Int = Math.round(Browser.window.innerWidth); //вычисляем ширину окна
+	var maxYPosition:Int = Math.round(Browser.window.innerHeight); // вычисляем высоту окна
+	
+	var myParent:Int = 0;
+	
+	bums(myParent, count, maxXPosition, maxYPosition);	
+	
+	
+}
+public static function bums (myParent:Int, count:Int, maxXPosition:Int, maxYPosition:Int){
+	var kubs:Array<Kvadrat> = new Array();
+	for (i in 0...count) {
+		var x1:Int = Std.random(maxXPosition);
+		var x2:Int = x1 + 50;
+		var y1:Int = Std.random(maxYPosition);
+		var y2:Int = y1 + 50;
+		if (count!=0){
+			kubs[i] = new Kvadrat(x1, y1, x2, y2, myParent);
+			openfl.Lib.current.addChild(kubs[i]);
+			bums(kubs[i].number, count-1, maxXPosition, maxYPosition); //рекурсивный вызов 
 		}
-		
-		//var k1:Kvadrat = new Kvadrat(50, 50, 100, 100);
-		//var k2:Kvadrat = new Kvadrat(50, 150, 250, 350);
-		//var k3:Kvadrat = new Kvadrat(200, 50, 250, 100);
-		
-		//openfl.Lib.current.addChild(k1);
-		//k1.addChild(k2);
-		//openfl.Lib.current.addChild(k3);
 	}
+}
 }
 
 class Kvadrat extends Sprite {
-	public static var counter: Int = 0;
-	var number: Int;
-	var x1:Int;	var y1:Int;
-	var x2:Int;	var y2:Int;
+public static var counter: Int = 0;
+
+public var number: Int;
+var myParent: Int;
+var x1:Int;	var y1:Int;
+var x2:Int;	var y2:Int;
+
+public function new (x1:Int, y1:Int, x2:Int, y2:Int, myParent:Int){
+	super();
+	Kvadrat.counter++;
+	this.myParent = myParent;
+	this.number = Kvadrat.counter;
+	addEventListener(MouseEvent.MOUSE_DOWN, function(e){trace(this.number+" -> "+this.myParent); e.stopPropagation(); });
 	
-	public function new (x1:Int, y1:Int, x2:Int, y2:Int){
-		super();
-		Kvadrat.counter++;
-		this.number = Kvadrat.counter;
-		addEventListener(MouseEvent.MOUSE_DOWN, function(e){Browser.alert(this.number); e.stopPropagation(); });
-		
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2;
+	this.y2 = y2;
+
 	
-		//graphics.beginFill(0xFFAA00);
-		graphics.beginFill(Math.round(Math.random() * 0xFFFFFF));
-		graphics.moveTo(x1,y1); // Откуда будем рисовать,  Левый верхний угол
-		graphics.lineTo(x2,y1); // Правый верхний угол
-		graphics.lineTo(x2,y2); // Правый нижний угол
-		graphics.lineTo(x1,y2); // Левый нижний угол
-		graphics.endFill();	
-	}
-}
+	graphics.beginFill(Math.round(Math.random() * 0xFFFFFF));// рандомный цвет квадратов
+	graphics.moveTo(x1,y1); // Откуда будем рисовать,  Левый верхний угол
+	graphics.lineTo(x2,y1); // Правый верхний угол
+	graphics.lineTo(x2,y2); // Правый нижний угол
+	graphics.lineTo(x1,y2); // Левый нижний угол
+	graphics.endFill();	
+								}
+
+				}
